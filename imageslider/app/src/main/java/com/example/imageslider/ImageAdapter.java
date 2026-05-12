b7efebfd4bcb37bef44bfe7b036ea9bb54673a55
Koumudi Sahasrabudhe <koumudisahasrabudhe27@Koumudis-MacBook-Air.local>
2026-05-12 22:00:24 +0530

    Added AD_7 Image Slider

---
+++ imageslider/app/src/main/java/com/example/imageslider/ImageAdapter.java
@@ -0,0 +1,51 @@
+package com.example.imageslider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+
+    private int[] images;
+
+    public ImageAdapter(int[] images) {
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
