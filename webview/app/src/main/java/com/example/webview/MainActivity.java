f61733386554c8e79fc9f23ebe53f81a38375754
Koumudi Sahasrabudhe <koumudisahasrabudhe27@Koumudis-MacBook-Air.local>
2026-05-12 21:19:44 +0530

    Added AD_6 WebView App

---
+++ webview/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.webview;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);   // Important
+        webSettings.setDomStorageEnabled(true);   // Helps loading
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://www.google.com");
+    }
+
+    // Back button support
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
